875edfa81dfd924af42ea3554766a255a272a246
vagrant <vagrant@lucid32.hsd1.ca.comcast.net>
2013-07-01 23:22:22 +0200

    Init repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RenanCodeCom::Application.config.secret_token = '56dd657002a5e234fc9c65eab369724108bb4e44dabafe73d06ae5e3c44ba9c7efa35ce6580ed7c555f04bee230bd15f895aea2cf7fc92afa97992b04e7dfa07'
