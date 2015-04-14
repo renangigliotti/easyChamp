c07d7d88b10ea9e9d13b49de58bfcf8617a4d18b
Renan Gigliotti <renangigliotti@gmail.com>
2015-04-14 00:10:41 -0300

    Ajustando indices

--- db/migrate/20130705154343_add_index_to_tables.rb
+++ db/migrate/20130705154343_add_index_to_tables.rb
@@ -1,16 +1,16 @@
 class AddIndexToTables < ActiveRecord::Migration
   def change
-     add_index :championships, :id
-     add_index :games, :id
-     add_index :phases, :id
-     add_index :rules, :id
-     add_index :teams, :id
+     add_index(:championships, :id)
+     add_index(:games, :id)
+     add_index(:phases, :id)
+     add_index(:rules, :id)
+     add_index(:teams, :id)
 
      add_foreign_key(:championships, :rules)
      add_foreign_key(:teams, :championships)
      add_foreign_key(:games, :championships)
      add_foreign_key(:games, :phases)
-     add_foreign_key(:games, :teams1)
-     add_foreign_key(:games, :teams2)
+     add_foreign_key(:games, :teams, column: :team1_id)
+     add_foreign_key(:games, :teams, column: :team2_id)
   end
 end
